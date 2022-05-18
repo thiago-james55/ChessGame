5217926c3ef64b554b6125ca1bd240343100a9f1
Thiago Gonçalves Souza <tsyhwh55@gmail.com>
2022-05-18 19:55:09 -0300

    Implementado função que seleciona (Highlight) as posições possiveis de cada peça após a mesma ser selecionada.

--- src/chess/ChessMatch.java
+++ src/chess/ChessMatch.java
@@ -43,6 +43,12 @@ public class ChessMatch {
         return mat;
     }
 
+    public boolean[][] possibleMoves(ChessPosition sourcePosition) {
+         Position position = sourcePosition.toPosition();
+         validateSourcePosition(position);
+         return board.piece(position).possibleMoves();
+    }
+
     public ChessPiece performChessMove (ChessPosition sourcePosition,ChessPosition targetPosition) {
 
         Position source = sourcePosition.toPosition();
@@ -75,7 +81,6 @@ public class ChessMatch {
         }
 
     }
-
 
 
     private void placeNewPiece(char column, int row, ChessPiece piece) {
